3c99859950e2c1f4578110358af2e71a530e82ae
Edi Spring <edi-tech@lambda-it.ch>
2017-11-23 15:07:29 +0100

    more tests

---
+++ tests/testfiles/6629-rdf.js
@@ -0,0 +1,55 @@
+exports.rdf = `<rdf:RDF
+  xmlns:ns1="http://www.mediathek.admin.ch/zem/"
+  xmlns:ns2="http://purl.org/dc/elements/1.1/"
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:ns3="http://zem.ch/"
+>
+  <rdf:Description rdf:about="file:///D:/projects/rdflib-service/app/">
+    <ns2:rights>
+      <rdf:Bag rdf:nodeID="f032e9f1e80ea4232b554756240399dd0b1">
+        <rdf:_1 xml:lang="de">armasuisse</rdf:_1>
+        <rdf:_2 xml:lang="fr">fr_armasuisse</rdf:_2>
+      </rdf:Bag>
+    </ns2:rights>
+    <ns1:Produzent>armasuisse</ns1:Produzent>
+    <ns1:Produktions-Nr>RP2010</ns1:Produktions-Nr>
+    <ns2:title>
+      <rdf:Alt rdf:nodeID="f032e9f1e80ea4232b554756240399dd0b3">
+        <rdf:_1 xml:lang="de">Iveco Lastwagen, 4x4, geländegängig, Brücke mit Verdeck</rdf:_1>
+        <rdf:_2 xml:lang="fr">fr_Iveco Lastwagen, 4x4, geländegängig, Brücke mit Verdeck</rdf:_2>
+      </rdf:Alt>
+    </ns2:title>
+    <ns1:AssetType>PIC</ns1:AssetType>
+    <ns1:Status>in work</ns1:Status>
+    <ns3:AssetId>e94f4505-cf41-4382-b0b0-1107f3dd4be1</ns3:AssetId>
+    <ns1:Medientraeger>Digital</ns1:Medientraeger>
+    <ns2:description>
+      <rdf:Alt rdf:nodeID="f032e9f1e80ea4232b554756240399dd0b2">
+        <rdf:_2 xml:lang="fr">fr_Rüstungsprogramm 2010</rdf:_2>
+        <rdf:_1 xml:lang="de">Rüstungsprogramm 2010</rdf:_1>
+      </rdf:Alt>
+    </ns2:description>
+    <ns1:Medientyp_Format>Querformat</ns1:Medientyp_Format>
+    <ns2:language>de</ns2:language>
+    <ns1:Publiziert>2017-23-11</ns1:Publiziert>
+    <ns1:Produziert>2017-23-11</ns1:Produziert>
+    <ns3:AssetState>mandant</ns3:AssetState>
+    <ns3:Filename>6629_222.jpg</ns3:Filename>
+    <ns1:Nutzung>spezial</ns1:Nutzung>
+    <ns2:format>jpeg</ns2:format>
+    <ns2:subject>
+      <rdf:Bag rdf:nodeID="f032e9f1e80ea4232b554756240399dd0b5">
+        <rdf:_4 xml:lang="en">armed forces organisation</rdf:_4>
+        <rdf:_3 xml:lang="it">organizzazione dell'esercito</rdf:_3>
+        <rdf:_2 xml:lang="fr">organisation de l'armÃ©e</rdf:_2>
+        <rdf:_1 xml:lang="de">Organisation der Armee</rdf:_1>
+      </rdf:Bag>
+    </ns2:subject>
+    <ns2:creator>
+      <rdf:Seq rdf:nodeID="f032e9f1e80ea4232b554756240399dd0b4">
+        <rdf:_1>unbekannt</rdf:_1>
+      </rdf:Seq>
+    </ns2:creator>
+    <ns1:Auftraggeber>Stab Chef der Armee (Stab CdA)</ns1:Auftraggeber>
+  </rdf:Description>
+</rdf:RDF>`;
---
+++ tests/testfiles/keller-rdf.js
@@ -0,0 +1,241 @@
+exports.rdf = `<rdf:RDF
+  xmlns:ns1="http://ns.adobe.com/camera-raw-settings/1.0/"
+  xmlns:ns7="http://ns.adobe.com/exif/1.0/aux/"
+  xmlns:ns5="http://ns.adobe.com/xap/1.0/mm/"
+  xmlns:ns4="http://www.mediathek.admin.ch/zem/"
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:ns6="http://ns.adobe.com/xap/1.0/sType/ResourceEvent#"
+  xmlns:xmp="http://ns.adobe.com/xap/1.0/"
+  xmlns:dc="http://purl.org/dc/elements/1.1/"
+  xmlns:ns9="http://digame.born.ch/"
+  xmlns:photoshop="http://ns.adobe.com/photoshop/1.0/"
+  xmlns:ns8="http://ns.adobe.com/xap/1.0/sType/ResourceRef#"
+>
+    <rdf:Description rdf:about="file:///app/">
+        <ns1:ToneCurve>
+            <rdf:Seq rdf:nodeID="ub1bL105C19">
+                <rdf:_1>0, 0</rdf:_1>
+                <rdf:_2>255, 255</rdf:_2>
+            </rdf:Seq>
+        </ns1:ToneCurve>
+        <photoshop:DateCreated>2016-10-21T16:10:57.12</photoshop:DateCreated>
+        <ns1:UprightTransformCount>6</ns1:UprightTransformCount>
+        <ns1:UprightCenterNormX>0.5</ns1:UprightCenterNormX>
+        <ns7:ImageNumber>7854</ns7:ImageNumber>
+        <ns1:ToneCurveName2012>Linear</ns1:ToneCurveName2012>
+        <ns1:BlueHue>0</ns1:BlueHue>
+        <ns1:HasCrop>False</ns1:HasCrop>
+        <ns1:Sharpness>81</ns1:Sharpness>
+        <ns1:GreenSaturation>0</ns1:GreenSaturation>
+        <ns1:LuminanceAdjustmentGreen>0</ns1:LuminanceAdjustmentGreen>
+        <ns9:Filename>6565_240.jpg</ns9:Filename>
+        <ns1:CameraProfile>Adobe Standard</ns1:CameraProfile>
+        <ns1:SharpenRadius>+1.0</ns1:SharpenRadius>
+        <ns1:DefringeGreenHueHi>60</ns1:DefringeGreenHueHi>
+        <ns1:LuminanceAdjustmentYellow>0</ns1:LuminanceAdjustmentYellow>
+        <ns1:Tint>-2</ns1:Tint>
+        <ns1:Version>9.7</ns1:Version>
+        <ns1:SaturationAdjustmentGreen>0</ns1:SaturationAdjustmentGreen>
+        <ns1:HueAdjustmentGreen>0</ns1:HueAdjustmentGreen>
+        <ns1:UprightPreview>False</ns1:UprightPreview>
+        <ns1:SaturationAdjustmentBlue>0</ns1:SaturationAdjustmentBlue>
+        <ns7:LensInfo>700/10 2000/10 28/10 28/10</ns7:LensInfo>
+        <ns1:LensProfileEnable>0</ns1:LensProfileEnable>
+        <ns1:SplitToningHighlightHue>0</ns1:SplitToningHighlightHue>
+        <dc:format>image/jpeg</dc:format>
+        <ns4:Medientyp_Format>Querformat</ns4:Medientyp_Format>
+        <ns1:SaturationAdjustmentAqua>0</ns1:SaturationAdjustmentAqua>
+        <ns1:Whites2012>0</ns1:Whites2012>
+        <ns4:Produktions-Nr>6565</ns4:Produktions-Nr>
+        <ns1:PerspectiveScale>100</ns1:PerspectiveScale>
+        <ns4:Medientraeger>Digital</ns4:Medientraeger>
+        <ns1:ColorNoiseReduction>25</ns1:ColorNoiseReduction>
+        <ns1:DefringePurpleHueHi>70</ns1:DefringePurpleHueHi>
+        <ns1:PerspectiveX>0.00</ns1:PerspectiveX>
+        <ns1:HueAdjustmentRed>0</ns1:HueAdjustmentRed>
+        <ns1:SaturationAdjustmentRed>0</ns1:SaturationAdjustmentRed>
+        <ns1:Shadows2012>+63</ns1:Shadows2012>
+        <ns1:LuminanceAdjustmentOrange>0</ns1:LuminanceAdjustmentOrange>
+        <ns5:OriginalDocumentID>7C5E9F787695AF23E911EE99779FF80D</ns5:OriginalDocumentID>
+        <ns1:Exposure2012>+0.75</ns1:Exposure2012>
+        <dc:description>
+            <rdf:Alt rdf:nodeID="ub1bL172C20">
+                <rdf:_1>Dies ist ein Test@de</rdf:_1>
+            </rdf:Alt>
+        </dc:description>
+        <ns9:AssetId>114984</ns9:AssetId>
+        <ns1:SplitToningShadowSaturation>0</ns1:SplitToningShadowSaturation>
+        <ns1:Highlights2012>-54</ns1:Highlights2012>
+        <ns1:SaturationAdjustmentYellow>0</ns1:SaturationAdjustmentYellow>
+        <ns1:Blacks2012>0</ns1:Blacks2012>
+        <ns1:ToneCurveName>Linear</ns1:ToneCurveName>
+        <ns1:WhiteBalance>As Shot</ns1:WhiteBalance>
+        <ns1:Saturation>+32</ns1:Saturation>
+        <ns1:SharpenEdgeMasking>39</ns1:SharpenEdgeMasking>
+        <ns1:SplitToningBalance>0</ns1:SplitToningBalance>
+        <ns1:LuminanceAdjustmentBlue>0</ns1:LuminanceAdjustmentBlue>
+        <ns1:UprightFocalMode>0</ns1:UprightFocalMode>
+        <xmp:ModifyDate>2016-10-26T11:07:07+02:00</xmp:ModifyDate>
+        <xmp:MetadataDate>2016-10-26T11:07:07+02:00</xmp:MetadataDate>
+        <ns1:PerspectiveY>0.00</ns1:PerspectiveY>
+        <ns1:Vibrance>+30</ns1:Vibrance>
+        <ns1:UprightFourSegmentsCount>0</ns1:UprightFourSegmentsCount>
+        <ns1:LuminanceAdjustmentPurple>0</ns1:LuminanceAdjustmentPurple>
+        <ns1:SaturationAdjustmentOrange>0</ns1:SaturationAdjustmentOrange>
+        <ns4:Produzent>VBS/DDPS - ZEM</ns4:Produzent>
+        <ns1:PostCropVignetteAmount>0</ns1:PostCropVignetteAmount>
+        <ns1:LuminanceAdjustmentRed>0</ns1:LuminanceAdjustmentRed>
+        <ns1:AutoWhiteVersion>134348800</ns1:AutoWhiteVersion>
+        <ns1:ConvertToGrayscale>False</ns1:ConvertToGrayscale>
+        <xmp:CreatorTool>Adobe Photoshop Lightroom 6.7 (Macintosh)</xmp:CreatorTool>
+        <ns1:HueAdjustmentBlue>0</ns1:HueAdjustmentBlue>
+        <ns1:ParametricShadows>0</ns1:ParametricShadows>
+        <ns1:SharpenDetail>25</ns1:SharpenDetail>
+        <ns1:ParametricMidtoneSplit>50</ns1:ParametricMidtoneSplit>
+        <ns1:Clarity2012>+50</ns1:Clarity2012>
+        <ns1:LensProfileSetup>LensDefaults</ns1:LensProfileSetup>
+        <ns1:ToneCurvePV2012>
+            <rdf:Seq rdf:nodeID="ub1bL116C25">
+                <rdf:_2>255, 255</rdf:_2>
+                <rdf:_1>0, 0</rdf:_1>
+            </rdf:Seq>
+        </ns1:ToneCurvePV2012>
+        <ns1:ProcessVersion>6.7</ns1:ProcessVersion>
+        <ns7:ApproximateFocusDistance>150/10</ns7:ApproximateFocusDistance>
+        <ns1:GrainAmount>0</ns1:GrainAmount>
+        <ns1:SaturationAdjustmentMagenta>0</ns1:SaturationAdjustmentMagenta>
+        <xmp:CreateDate>2016-10-21T16:10:57.12</xmp:CreateDate>
+        <ns1:ToneCurvePV2012Blue>
+            <rdf:Seq rdf:nodeID="ub1bL119C29">
+                <rdf:_1>0, 0</rdf:_1>
+                <rdf:_2>255, 255</rdf:_2>
+            </rdf:Seq>
+        </ns1:ToneCurvePV2012Blue>
+        <ns1:Contrast2012>+10</ns1:Contrast2012>
+        <ns1:ParametricHighlights>0</ns1:ParametricHighlights>
+        <ns1:ParametricLights>0</ns1:ParametricLights>
+        <ns1:Dehaze>0</ns1:Dehaze>
+        <ns1:SplitToningShadowHue>0</ns1:SplitToningShadowHue>
+        <ns1:HueAdjustmentPurple>0</ns1:HueAdjustmentPurple>
+        <ns1:ParametricShadowSplit>25</ns1:ParametricShadowSplit>
+        <ns4:Publiziert>2016-11-11</ns4:Publiziert>
+        <ns1:HueAdjustmentOrange>0</ns1:HueAdjustmentOrange>
+        <ns1:Temperature>5100</ns1:Temperature>
+        <ns1:HasSettings>True</ns1:HasSettings>
+        <dc:rights>
+            <rdf:Alt rdf:nodeID="ub1bL175C15">
+                <rdf:_1 xml:lang="de">VBS/DDPS - ZEM</rdf:_1>
+            </rdf:Alt>
+        </dc:rights>
+        <ns5:DerivedFrom>
+            <rdf:Description rdf:nodeID="ub1bL157C21">
+                <ns8:documentID>7C5E9F787695AF23E911EE99779FF80D</ns8:documentID>
+                <ns8:originalDocumentID>7C5E9F787695AF23E911EE99779FF80D</ns8:originalDocumentID>
+            </rdf:Description>
+        </ns5:DerivedFrom>
+        <ns1:HueAdjustmentYellow>0</ns1:HueAdjustmentYellow>
+        <ns1:HueAdjustmentMagenta>0</ns1:HueAdjustmentMagenta>
+        <ns4:Produziert>2016-10-22</ns4:Produziert>
+        <ns1:PerspectiveAspect>0</ns1:PerspectiveAspect>
+        <ns1:ToneCurvePV2012Red>
+            <rdf:Seq rdf:nodeID="ub1bL125C28">
+                <rdf:_1>0, 0</rdf:_1>
+                <rdf:_2>255, 255</rdf:_2>
+            </rdf:Seq>
+        </ns1:ToneCurvePV2012Red>
+        <ns1:DefringeGreenAmount>0</ns1:DefringeGreenAmount>
+        <ns1:HueAdjustmentAqua>0</ns1:HueAdjustmentAqua>
+        <ns1:ParametricHighlightSplit>75</ns1:ParametricHighlightSplit>
+        <ns1:ToneCurveGreen>
+            <rdf:Seq rdf:nodeID="ub1bL111C24">
+                <rdf:_1>0, 0</rdf:_1>
+                <rdf:_2>255, 255</rdf:_2>
+            </rdf:Seq>
+        </ns1:ToneCurveGreen>
+        <ns1:ToneCurveBlue>
+            <rdf:Seq rdf:nodeID="ub1bL108C23">
+                <rdf:_1>0, 0</rdf:_1>
+                <rdf:_2>255, 255</rdf:_2>
+            </rdf:Seq>
+        </ns1:ToneCurveBlue>
+        <ns1:ToneCurvePV2012Green>
+            <rdf:Seq rdf:nodeID="ub1bL122C30">
+                <rdf:_1>0, 0</rdf:_1>
+                <rdf:_2>255, 255</rdf:_2>
+            </rdf:Seq>
+        </ns1:ToneCurvePV2012Green>
+        <ns1:PerspectiveHorizontal>0</ns1:PerspectiveHorizontal>
+        <ns7:SerialNumber>6043966</ns7:SerialNumber>
+        <ns4:Status>accepted</ns4:Status>
+        <ns1:LuminanceAdjustmentAqua>0</ns1:LuminanceAdjustmentAqua>
+        <ns1:DefringePurpleHueLo>30</ns1:DefringePurpleHueLo>
+        <ns1:DefringePurpleAmount>0</ns1:DefringePurpleAmount>
+        <ns1:BlueSaturation>0</ns1:BlueSaturation>
+        <ns5:InstanceID>xmp.iid:079c9694-c04f-4cf1-99ed-d71b5c54ada7</ns5:InstanceID>
+        <ns1:LensManualDistortionAmount>0</ns1:LensManualDistortionAmount>
+        <ns7:LensID>162</ns7:LensID>
+        <ns1:LuminanceSmoothing>0</ns1:LuminanceSmoothing>
+        <ns1:UprightFocalLength35mm>35</ns1:UprightFocalLength35mm>
+        <ns1:UprightCenterMode>0</ns1:UprightCenterMode>
+        <ns1:PerspectiveUpright>0</ns1:PerspectiveUpright>
+        <ns1:SplitToningHighlightSaturation>0</ns1:SplitToningHighlightSaturation>
+        <ns1:AlreadyApplied>True</ns1:AlreadyApplied>
+        <dc:creator>
+            <rdf:Seq rdf:nodeID="ub1bL170C16">
+                <rdf:_1>Alexander Kühni / Thomas Gerber</rdf:_1>
+            </rdf:Seq>
+        </dc:creator>
+        <ns1:SaturationAdjustmentPurple>0</ns1:SaturationAdjustmentPurple>
+        <ns1:UprightVersion>151388160</ns1:UprightVersion>
+        <ns1:AutoLateralCA>1</ns1:AutoLateralCA>
+        <ns1:PerspectiveVertical>0</ns1:PerspectiveVertical>
+        <ns1:ColorNoiseReductionSmoothness>50</ns1:ColorNoiseReductionSmoothness>
+        <ns1:ToneMapStrength>0</ns1:ToneMapStrength>
+        <dc:title>
+            <rdf:Alt rdf:nodeID="ub1bL177C14">
+                <rdf:_1 xml:lang="de">Panzer 87 Leopard (Pz 87 Leo) in Fahrt</rdf:_1>
+            </rdf:Alt>
+        </dc:title>
+        <ns7:Lens>70.0-200.0 mm f/2.8</ns7:Lens>
+        <ns1:GreenHue>0</ns1:GreenHue>
+        <ns4:AssetType>PIC</ns4:AssetType>
+        <ns1:ToneCurveRed>
+            <rdf:Seq rdf:nodeID="ub1bL128C22">
+                <rdf:_1>0, 0</rdf:_1>
+                <rdf:_2>255, 255</rdf:_2>
+            </rdf:Seq>
+        </ns1:ToneCurveRed>
+        <ns1:LuminanceAdjustmentMagenta>0</ns1:LuminanceAdjustmentMagenta>
+        <ns1:ColorNoiseReductionDetail>50</ns1:ColorNoiseReductionDetail>
+        <ns1:UprightCenterNormY>0.5</ns1:UprightCenterNormY>
+        <ns4:Nutzung>freie Nutzung</ns4:Nutzung>
+        <ns5:DocumentID>xmp.did:079c9694-c04f-4cf1-99ed-d71b5c54ada7</ns5:DocumentID>
+        <ns1:ShadowTint>0</ns1:ShadowTint>
+        <ns1:ParametricDarks>0</ns1:ParametricDarks>
+        <ns1:PerspectiveRotate>0.0</ns1:PerspectiveRotate>
+        <ns1:VignetteAmount>0</ns1:VignetteAmount>
+        <ns5:History>
+            <rdf:Seq rdf:nodeID="ub1bL160C17">
+                <rdf:_2>
+                    <rdf:Description rdf:nodeID="ub1bL163C20">
+                        <ns6:changed>/</ns6:changed>
+                        <ns6:softwareAgent>Adobe Photoshop Lightroom 6.7 (Macintosh)</ns6:softwareAgent>
+                        <ns6:instanceID>xmp.iid:079c9694-c04f-4cf1-99ed-d71b5c54ada7</ns6:instanceID>
+                        <ns6:when>2016-10-26T11:07:07+02:00</ns6:when>
+                        <ns6:action>saved</ns6:action>
+                    </rdf:Description>
+                </rdf:_2>
+                <rdf:_1>
+                    <rdf:Description rdf:nodeID="ub1bL161C20">
+                        <ns6:parameters>converted from image/x-nikon-nef to image/jpeg, saved to new location</ns6:parameters>
+                        <ns6:action>derived</ns6:action>
+                    </rdf:Description>
+                </rdf:_1>
+            </rdf:Seq>
+        </ns5:History>
+        <ns1:DefringeGreenHueLo>40</ns1:DefringeGreenHueLo>
+        <ns1:RedSaturation>0</ns1:RedSaturation>
+        <ns1:RedHue>0</ns1:RedHue>
+        <ns4:Auftraggeber>Heer</ns4:Auftraggeber>
+    </rdf:Description>
+</rdf:RDF>`;
